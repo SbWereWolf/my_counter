bc4024daa302041e24d3862f1c6d18477e5283d3
SbWereWolf <SbWereWolf@users.noreply.github.com>
2023-09-03 21:17:20 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -314,6 +314,7 @@ declare class $my_counter extends $my_lom_view {
     _Count: $my_lom_view;
     Count(): $my_lom_view | $my_lom_input;
     sub(): ($my_lom_view | $my_lom_button | $my_lom_input)[];
+    static mount(): void;
 }
 
 declare namespace $ {
--- node.js
+++ node.js
@@ -1114,8 +1114,16 @@ class $my_counter extends $my_lom_view {
             this.Inc(),
         ];
     }
+    static mount() {
+        if (typeof document === 'undefined')
+            return;
+        const node = document.querySelector('#root');
+        const obj = new $my_counter();
+        node?.replaceWith(obj.dom_tree());
+        setInterval(() => obj.dom_tree(), 100);
+    }
 }
-$my_lom_view.root = () => $my_counter;
+$my_counter.mount();
 //my/counter/counter.ts
 ;
 "use strict";
